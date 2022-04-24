a2b97bf8d745941e4fdf0b338593fcf0b181442f
Santiago Martin Berretta Gali <santiagomartinbg@hotmail.com>
2022-04-24 13:29:09 -0300

    Agregado ejercicio 1 del tema 7

---
+++ Proyectos/Python/EjerciciosTema7/main.py
@@ -0,0 +1,9 @@
+import calculadora
+
+valor1=int(input("Ingrese el primer valor "))
+valor2=int(input("Ingrese el segundo valor "))
+
+print("la suma es ",calculadora.suma(valor1,valor2))
+print("la resta es ", calculadora.resta(valor1,valor2))
+print("la multiplicacion es ", calculadora.multiplicacion(valor1,valor2))
+print("la division es ", calculadora.division(valor1,valor2))
