286634a786df28afec42dd7ec73d9f09889fb1b9
Santiago Martin Berretta Gali <santiagomartinbg@hotmail.com>
2022-05-03 12:28:31 -0300

    Añadido ejercicio 2

--- Proyectos/Python/EjerciciosTema8/Ejercicio2.py
+++ Proyectos/Python/EjerciciosTema8/Ejercicio2.py
@@ -1,6 +1,7 @@
 # En este segundo ejercicio, tendréis que crear un archivo py y dentro crearéis una clase Vehículo,
 # haréis un objeto
 # de ella, lo guardaréis en un archivo y luego lo cargamos.
+
 import pickle
 
 class Vehiculo:
